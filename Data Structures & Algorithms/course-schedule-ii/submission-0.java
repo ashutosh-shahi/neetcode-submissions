2b45f4eb9ada4681248cb5776b01c5c5c4293b7f
Ashutosh-Shahi <shahiashu.india@gmail.com>
2026-09-10 01:54:50 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.java
@@ -0,0 +1,53 @@
+// class Solution {
+//     public int[] findOrder(int numCourses, int[][] prerequisites) {
+        
+//     }
+// }
+
+
+class Solution {
+    public int[] findOrder(int num, int[][] nums) {
+        int n = nums.length;
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < num; i++ ) {
+            adj.add(new ArrayList<>());
+        }
+        for(int i = 0; i < nums.length; i++) {
+            int a = nums[i][0];
+            int b = nums[i][1];
+            adj.get(b).add(a);
+        }
+        List<Integer> list = new ArrayList<>();
+        int[] indegree = new int[num];
+        for(int i = 0; i < num; i++) {
+            for(int it: adj.get(i)) {
+                indegree[it]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < num; i++) {
+            if(indegree[i] == 0) {
+                q.add(i);
+                list.add(i);
+            }
+        }
+        while(!q.isEmpty()) {
+            int tmp = q.poll();
+            for(int it: adj.get(tmp)) {
+                indegree[it]--;
+                if(indegree[it] == 0) {
+                    q.add(it);
+                    list.add(it);
+                }
+            }
+        }
+        for(int i = 0; i < num; i++) {
+            if(indegree[i] != 0) return new int[]{};
+        }
+        int[] arr = new int[list.size()];
+        for(int i = 0; i < list.size(); i++) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+}
